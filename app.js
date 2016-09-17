c7c4dfae2174ab7ad1ae53ab8baf654126f85573
Mafooch <joshuamaffuccio@gmail.com>
2016-09-17 15:00:47 -0400

    Implement working functionality

--- app.js
+++ app.js
@@ -6,6 +6,28 @@
 
   LunchCheckController.inject = ["$scope"];
   function LunchCheckController($scope) {
-    $scope.name = "josh";
+    $scope.numberOfItems = "";
+    $scope.dishes = "";
+    $scope.message = "";
+    $scope.checkTooMuch = function() {
+      var dishArray = $scope.dishes.split(",");
+      var sanitizedDishArray = sanitizeArray(dishArray);
+      if(sanitizedDishArray.length > 3) {
+        $scope.message = "Too much";
+      } else if(sanitizedDishArray.length === 0) {
+        $scope.message = "Please enter data first!";
+      } else {
+        $scope.message = "Enjoy!";
+      }
+    };
+  }
+  function sanitizeArray(array) {
+    var sanitizedArray = [];
+    for(var i = 0; i < array.length; i++) {
+      if(array[i].trim() !== "") {
+        sanitizedArray.push(array[i]);
+      }
+    }
+    return sanitizedArray;
   }
 })();
